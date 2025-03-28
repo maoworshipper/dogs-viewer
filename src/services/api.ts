81196122c17359858c5b38c3dbd8c946b150edd2
MauricioMartinez <maoworshipper@gmail.com>
2025-03-27 19:41:46 -0500

    update api service to improve data fetching and error handling

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,47 @@
-import { IDataFetch } from "../types/dataTypes";
+import { getFormattedData } from "../utils/common";
+import {
+  IDataResponse,
+  IPokemonDetail,
+  IAbilityDetails,
+} from "../types/dataTypes";
 
-export const getData = async ({ page = 1, limit = 10 }: IDataFetch) => {
-  const apiURL = "https://pokeapi.co/api/v2/pokemon";
-  const response = await fetch(
-    `${apiURL}?offset=${(page - 1) * limit}&limit=${limit}`
+const BASE_URL = "https://pokeapi.co/api/v2";
+
+async function fetchData<T>(url: string): Promise<T> {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Error fetching data: ${response.status}`);
+    }
+    const data: T = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    if (error instanceof Error) {
+      throw new Error(`Failed to fetch from ${url}: ${error.message}`);
+    }
+    throw new Error(`Error fetching from ${url}`);
+  }
+}
+
+export const fetchAllItems = async (
+  limit: number = 1400
+): Promise<IDataResponse> => {
+  const data = await fetchData<IDataResponse>(
+    `${BASE_URL}/pokemon?limit=${limit}&offset=0`
   );
-  return response.json();
+
+  return getFormattedData(data);
+};
+
+export const fetchItemDetails = async (
+  url: string
+): Promise<IPokemonDetail> => {
+  return fetchData<IPokemonDetail>(url);
+};
+
+export const fetchAbilityDetails = async (
+  url: string
+): Promise<IAbilityDetails> => {
+  return fetchData<IAbilityDetails>(url);
 };
